e8b9a3f1d5bb5fc1e909bc5c5e0876869da3057d
Artyom Sheludeshev <sheludeshev.artyom@mail.ru>
2018-09-21 03:53:53 +0300

    Сверстал welcome page, оптимизаций не делал

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,1 +1,21 @@
-console.log("auth module loaded");
+(function () {
+  'use strict';
+
+  let authButton = document.querySelector('.auth-button');
+  let backButton = document.querySelector('.menu__navigation__button[type="button"]');
+  let menuPrev = document.querySelector('.menu-preview');
+  let menuAuth = document.querySelector('.menu-authorization');
+
+  authButton.addEventListener('click', () => {
+    authButton.classList.add('auth-button--hidden');
+    menuPrev.classList.remove('menu-preview--active');
+    menuAuth.classList.add('menu-authorization--active');
+  });
+
+  backButton.addEventListener('click', () => {
+    authButton.classList.remove('auth-button--hidden');
+    menuPrev.classList.add('menu-preview--active');
+    menuAuth.classList.remove('menu-authorization--active');
+  });
+  
+})();
