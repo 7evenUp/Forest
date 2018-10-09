88cbc3aa2d985ccba1b2cfb855d1cd7b9cb3543a
Artyom Sheludeshev <sheludeshev.artyom@mail.ru>
2018-10-09 12:06:43 +0300

    Исправил ширину футера при большой ширине экрана. Доработал sidebar блога.

--- src/assets/scripts/modules/google-map.js
+++ src/assets/scripts/modules/google-map.js
@@ -1,4 +1,18 @@
 var map;
+
+// var myStyles = [
+//   {
+//     featureType: 'water',
+//     elementType: 'geometry',
+//     stylers: [{color: '#6c9c5a'}]
+//   },
+//   {
+//     featureType: 'water',
+//     elementType: 'geometry',
+//     stylers: [{color: '#373e42'}]
+//   }
+// ];
+
 var initMap =(function () {
   
   var coordinates = {lat: 60.048339, lng: 30.322016},
@@ -12,7 +26,24 @@ var initMap =(function () {
 
   marker = new google.maps.Marker({
     position: {lat: 60.047709, lng: 30.334475},
-    map: map
+    map: map,
+    styles: [
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{color: '#6c9c5a'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry.fill',
+        stylers: [{color: '#6c9c5a'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#373e42'}]
+      }
+    ]
   })
 
 })();
